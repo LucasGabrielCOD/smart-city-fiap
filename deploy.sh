3fdb8de5374e6efa7391cae6f21d84dd3c640f54
lucas <lucasgabi_biel@hotmail.com>
2024-10-16 19:16:09 -0300

    [TESTE] Ajusta script de deploy para versão 0.0.2-alpha

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Verificamos se o ambiente foi especificado
+# Verifica se o ambiente foi especificado
 if [ -z "$1" ]; then
   echo "Uso: $0 <ambiente>"
   echo "Onde <ambiente> pode ser 'staging' ou 'production'"
   exit 1
 fi
 
-# Definimos o ambiente
+# Define o ambiente
 ambiente=$1
 
 # Faz o pull da imagem Docker
@@ -16,15 +16,9 @@ docker pull ${{ secrets.DOCKER_USERNAME }}/smart-city:latest
 # Define as variáveis de ambiente de acordo com o ambiente
 if [ "$ambiente" == "staging" ]; then
   export SPRING_PROFILES_ACTIVE=staging
-  export DB_URL=${{ secrets.DB_URL_STAGING }}
-  export DB_USER=${{ secrets.DB_USER_STAGING }}
-  export DB_PASSWORD=${{ secrets.DB_PASSWORD_STAGING }}
   service=smart-city-staging
 elif [ "$ambiente" == "production" ]; then
   export SPRING_PROFILES_ACTIVE=production
-  export DB_URL=${{ secrets.DB_URL_PRODUCTION }}
-  export DB_USER=${{ secrets.DB_USER_PRODUCTION }}
-  export DB_PASSWORD=${{ secrets.DB_PASSWORD_PRODUCTION }}
   service=smart-city-production
 else
   echo "Ambiente inválido: $ambiente"
