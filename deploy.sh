743e968e3da17beb3732b5d3dfa422146f51c1ad
lucas <lucasgabi_biel@hotmail.com>
2024-10-17 00:09:29 -0300

    [TESTE] Ajusta script de deploy para versão 0.0.37alpha

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,17 @@ echo "Valor do argumento: $1"
 if [ "$1" = "staging" ]; then
   export SPRING_PROFILES_ACTIVE=staging
   service=smart-city-staging
+  # Define as variáveis de ambiente para staging
+  export DB_URL=${DB_URL_STAGING}
+  export DB_USER=${DB_USER_STAGING}
+  export DB_PASSWORD=${DB_PASSWORD_STAGING}
 elif [ "$1" = "production" ]; then
   export SPRING_PROFILES_ACTIVE=production
   service=smart-city-production
+  # Define as variáveis de ambiente para production
+  export DB_URL=${DB_URL_PRODUCTION}
+  export DB_USER=${DB_USER_PRODUCTION}
+  export DB_PASSWORD=${DB_PASSWORD_PRODUCTION}
 else
   echo "Ambiente inválido: $1"
   exit 1
