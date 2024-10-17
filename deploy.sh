0f013652c52e77c6535bd638ee8f903400551566
lucas <lucasgabi_biel@hotmail.com>
2024-10-16 23:31:53 -0300

    [TESTE] Ajusta script de deploy para versão 0.0.31alpha

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,15 @@
 
 echo "Valor do argumento: $1"
 
-# Verifica se o ambiente foi especificado
-if [ -z "$1" ]; then
-  echo "Uso: $0 <ambiente>"
-  echo "Onde <ambiente> pode ser 'staging' ou 'production'"
-  exit 1
-fi
-
-# Define o ambiente
-export ambiente=$1  # Exportar a variável
-
-echo "Valor da variável ambiente: $ambiente"
-
-# Define as variáveis de ambiente de acordo com o ambiente
-if [ "$ambiente" = "staging" ]; then
+# Define as variáveis de ambiente de acordo com o argumento
+if [ "$1" = "staging" ]; then
   export SPRING_PROFILES_ACTIVE=staging
   service=smart-city-staging
-elif [ "$ambiente" = "production" ]; then
+elif [ "$1" = "production" ]; then
   export SPRING_PROFILES_ACTIVE=production
   service=smart-city-production
 else
-  echo "Ambiente inválido: $ambiente"
+  echo "Ambiente inválido: $1"
   exit 1
 fi
 
